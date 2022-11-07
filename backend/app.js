cc154c5236e9937801b4275a15d4be8eb9ed26e3
Imakhoy <hebadaoud09@gmail.com>
2022-11-07 10:57:16 +0100

    edit app.js et server.js

--- backend/app.js
+++ backend/app.js
@@ -1,64 +1,44 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet")
+const path = require("path");
 
-const express = require('express');
+//MIDDLEWARE
+const auth = require("./middleware/auth");
+
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const helmet = require('helmet');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 
-//DATABASE
-// Connecter Mongoose avec route MongoDB
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
-{ useNewUrlParser: true,
-  useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
 
+// HELMET
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } },
+{ crossOriginOpenerPolicy: { policy: "cross-origin" } }));
 
-  
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(helmet());
-
-// CORS ==> Cross Origin Ressource Sharing
-//Import du middleware qui permet d'accéder à l'API avec les méthodes pour envoyer les requêtes
+// CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+// Parse requests of content-type - application/json
+app.use(express.json());
 
-app.get("*", checkUser);
-app.get("/jwtid", requireAuth, (req, res) => {
-  res.status(200).json(res.locals.user);
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//JWT
+app.get("/jwtid", auth.requireAuth, (req, res) => {
+  res.status(200).send(req.auth.userId);
 });
 
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
+//ROUTES
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,64 +1,50 @@
+const http = require('http');
+const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('./config/db');
 
-// Importer package Node HTTP
-const http = require('http');
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-// Importer application Express
-const app = require('./app');
-
-
-// Renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
-
-const normalizePort = (val) => {
-        const port = parseInt(val, 10);    
-
-    if(isNaN(port)) {
+    if (isNaN(port)) {
         return val;
     }
-    if(port >= 0) {
+    if (port >= 0) {
         return port;
     }
-    return false;
+    return false
 };
+const port = normalizePort(process.env.PORT || '5000');
+app.set('port' , port);
 
-// Indiquer le port à l'application
-
-const port = normalizePort(process.env.PORT || "5000");
-app.set("port", port);
-
-// Recherche les différentes erreurs et les gère de manière appropriée
-// Elle est ensuite enregistré dans le serveur 
 const errorHandler = error => {
-    if(error.syscall !== 'listen') {
-      throw error;
+    if (error.syscall !== 'listen') {
+        throw error
     }
     const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port;
     switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
+        case 'EACCES' :
+            console.error(bind + 'requires elevated privileges.');
             process.exit(1)
             break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1)
+        case 'EADDRINUSE' : 
+            console.error(bind + 'is already in use');
+            process.exit(1);
             break;
         default:
-            throw error;
+            throw error
     }
 };
 
-// Créer server
 const server = http.createServer(app);
 
-// Un écouteur d'évènements, consigne le port ou le canal nommé sur lequel le serveur s'exécute dans la console
 server.on('error', errorHandler);
 server.on('listening', () => {
     const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port ' + port;
     console.log('Listening on ' + bind);
 });
 
-// Ecouter les requêtes sur le port
 server.listen(port);
