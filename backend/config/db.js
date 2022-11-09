8f4e093d1658f18fa87ec9827cd4018886ec12c8
Imakhoy <hebadaoud09@gmail.com>
2022-11-09 14:29:53 +0100

    edit db

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,7 @@ dotenv.config();
 
 //DATABASE
 // Connecter Mongoose avec route MongoDB
-//mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
-mongoose.connect("mongodb+srv://dbHeba:Mar_4569129@cluster0.zbkll.mongodb.net/groupomania?retryWrites=true&w=majority",
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 { useNewUrlParser: true,
   useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
