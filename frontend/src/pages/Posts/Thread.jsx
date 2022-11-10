023dd76a9b07a0302c7c7c6d95411782b7a51fa3
Imakhoy <hebadaoud09@gmail.com>
2022-11-10 16:30:48 +0100

    edit Thread

--- frontend/src/pages/Posts/Thread.jsx
+++ frontend/src/pages/Posts/Thread.jsx
@@ -1,13 +1,42 @@
 import { useDispatch } from "react-redux"
-import { Navigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
-const Thread = () => {
-  const dispatch = useDispatch()
+import { useEffect } from 'react'
+
+import { getUser } from './actions/user.actions'
+import { UidContext } from './components/AppContext'
+import Routes from './components/Routes'
 
 
-  return (
-    <div>{dispatch ? <div>POSTS</div> : <Navigate to={{ pathname: '/' }} />}</div>
-  )
-}
 
-export default Thread
+function Thread() {
+    const [uid, setUid] = useState(null)
+    const dispatch = useDispatch()
+    useEffect(() => {
+      const fetchToken = async () => {
+        await axios({
+          method: 'get',
+          url: `${process.env.REACT_APP_API_URL}jwtid`,
+          withCredentials: true,
+        })
+          .then((res) => {
+            setUid(res.data)
+          })
+          .catch((err) => {
+            console.log('No token')
+          })
+      }
+      fetchToken()
+      if (uid) dispatch(getUser(uid))
+  
+    }, [uid, dispatch])
+  
+    return (
+      <UidContext.Provider value={uid}>
+        <Routes />
+      </UidContext.Provider>
+    )
+  }
+  
+  export default Thread
