8c7fc265ba3970607b0eec30018af229032debbc
Imakhoy <hebadaoud09@gmail.com>
2022-11-10 09:45:03 +0100

    create post

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 const postController = require("../controllers/post.controller");
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-//const auth = require('../middleware/auth');
-//const multer = require('../middleware/multer-config');
-
-//CRUD POST
+// post display
 router.get('/', postController.getPosts);
-router.post('/', postController.addPosts);
-
-
-router.put('/:id', postController.updatePost);
-router.delete('/:id', postController.deletePost);
+router.post('/', auth, multer, postController.addPost);
+router.put('/:id', auth, multer, postController.updatePost);
+router.delete('/:id', auth, multer, postController.deletePost);
 //like 
-router.patch('/like-post/:id', postController.likePost);
+router.patch('/:id/like', auth, postController.likePost);
 router.patch('/unlike-post/:id', postController.unlikePost);
 
 module.exports = router;
